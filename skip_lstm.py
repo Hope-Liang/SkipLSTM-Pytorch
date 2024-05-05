9d45f4a434ce5519b09cf2b8cdb399c92101e360
Hope-Liang <hope-liang@sjtu.edu.cn>
2024-05-05 23:30:41 +0200

    bug fix

--- skip_lstm.py
+++ skip_lstm.py
@@ -10,57 +10,14 @@ import numpy as np
 
 
 class BinaryLayer(Function):
-    def forward(self, input):
-        return input.round()
- 
-    def backward(self, grad_output):
+    @staticmethod
+    def forward(ctx, input):
+        result = torch.round(input)
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
         return grad_output
-
-
-def SkipLSTMCell(input, hidden, num_layers, w_ih, w_hh, w_uh, b_ih=None, b_hh=None, b_uh=None,
-                  activation=F.tanh, lst_layer_norm=None):
-    if num_layers != 1:
-        raise RuntimeError("wrong num_layers: got {}, expected {}".format(num_layers, 1))
-    w_ih, w_hh = w_ih[0], w_hh[0]
-    b_ih = b_ih[0] if b_ih is not None else None
-    b_hh = b_hh[0] if b_hh is not None else None
-
-    c_prev, h_prev, update_prob_prev, cum_update_prob_prev = hidden
-
-    gates = F.linear(input, w_ih, b_ih) + F.linear(h_prev, w_hh, b_hh)
-
-    ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
-
-    if lst_layer_norm:
-        ingate = lst_layer_norm[0][0](ingate.contiguous())
-        forgetgate = lst_layer_norm[0][1](forgetgate.contiguous())
-        cellgate = lst_layer_norm[0][2](cellgate.contiguous())
-        outgate = lst_layer_norm[0][3](outgate.contiguous())
-
-    ingate = F.sigmoid(ingate)
-    forgetgate = F.sigmoid(forgetgate)
-    cellgate = activation(cellgate)
-    outgate = F.sigmoid(outgate)
-
-    new_c_tilde = (forgetgate * c_prev) + (ingate * cellgate)
-    new_h_tilde = outgate * activation(new_c_tilde)
-    # Compute value for the update prob
-    new_update_prob_tilde = F.sigmoid(F.linear(new_c_tilde, w_uh, b_uh))
-    # Compute value for the update gate
-    cum_update_prob = cum_update_prob_prev + torch.min(update_prob_prev, 1. - cum_update_prob_prev)
-    # round
-    bn = BinaryLayer()
-    update_gate = bn(cum_update_prob)
-    # Apply update gate
-    new_c = update_gate * new_c_tilde + (1. - update_gate) * c_prev
-    new_h = update_gate * new_h_tilde + (1. - update_gate) * h_prev
-    new_update_prob = update_gate * new_update_prob_tilde + (1. - update_gate) * update_prob_prev
-    new_cum_update_prob = update_gate * 0. + (1. - update_gate) * cum_update_prob
-
-    new_state = (new_c, new_h, new_update_prob, new_cum_update_prob)
-    new_output = (new_h, update_gate)
-
-    return new_output, new_state
 
 
 # Implementation from nn._functions.rnn.py
@@ -97,6 +54,52 @@ def BasicLSTMCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None,
     return hy, cy
 
 
+def SkipLSTMCell(input, hidden, num_layers, w_ih, w_hh, w_uh, b_ih=None, b_hh=None, b_uh=None,
+                  activation=F.tanh, lst_layer_norm=None):
+    if num_layers != 1:
+        raise RuntimeError("wrong num_layers: got {}, expected {}".format(num_layers, 1))
+
+    b_ih = b_ih[0] if b_ih is not None else None
+    b_hh = b_hh[0] if b_hh is not None else None
+
+    c_prev, h_prev, update_prob_prev, cum_update_prob_prev = hidden
+
+    gates = F.linear(input, w_ih, b_ih) + F.linear(h_prev, w_hh, b_hh)
+
+    ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+    if lst_layer_norm:
+        ingate = lst_layer_norm[0][0](ingate.contiguous())
+        forgetgate = lst_layer_norm[0][1](forgetgate.contiguous())
+        cellgate = lst_layer_norm[0][2](cellgate.contiguous())
+        outgate = lst_layer_norm[0][3](outgate.contiguous())
+
+    ingate = F.sigmoid(ingate)
+    forgetgate = F.sigmoid(forgetgate)
+    cellgate = activation(cellgate)
+    outgate = F.sigmoid(outgate)
+
+    new_c_tilde = (forgetgate * c_prev) + (ingate * cellgate)    
+    new_h_tilde = outgate * activation(new_c_tilde)
+    # Compute value for the update prob
+    new_update_prob_tilde = F.sigmoid(F.linear(new_c_tilde, w_uh, b_uh))
+    # Compute value for the update gate
+    cum_update_prob = cum_update_prob_prev + torch.min(update_prob_prev, 1. - cum_update_prob_prev)
+    # round
+    bn = BinaryLayer()
+    update_gate = bn.apply(cum_update_prob)
+    # Apply update gate
+    new_c = update_gate * new_c_tilde + (1. - update_gate) * c_prev
+    new_h = update_gate * new_h_tilde + (1. - update_gate) * h_prev
+    new_update_prob = update_gate * new_update_prob_tilde + (1. - update_gate) * update_prob_prev
+    new_cum_update_prob = update_gate * 0. + (1. - update_gate) * cum_update_prob
+
+    new_state = (new_c, new_h, new_update_prob, new_cum_update_prob)
+    new_output = (new_h, update_gate)
+
+    return new_output, new_state
+
+
 class CCellBase(RNNCellBase):
 
     def __init__(self, cell, learnable_elements, input_size, hidden_size, num_layers=1,
@@ -128,7 +131,7 @@ class CCellBase(RNNCellBase):
 class CCellBaseSkipLSTM(CCellBase):
 
     def __init__(self, cell, learnable_elements, input_size, hidden_size, num_layers=1,
-                    bias=True, batch_first = False, activation=F.tanh, layer_norm=False):
+                    bias=True, batch_first=False, activation=F.tanh, layer_norm=False):
         super(CCellBaseSkipLSTM, self).__init__(cell, learnable_elements, input_size, hidden_size, num_layers,
                                                 bias, batch_first, activation, layer_norm)
         self.weight_uh = Parameter(xavier_uniform_(torch.Tensor(1, hidden_size)))
@@ -155,22 +158,22 @@ class CCellBaseSkipLSTM(CCellBase):
                 else:
                     hx = [tuple([j.cuda() if j is not None else None for j in i]) for i in hx]
 
-        if len(input.shape) == 3:
-            self.check_forward_input(input[0])
-            if self.num_layers > 1:
-                self.check_forward_hidden(input[0], hx[0][0], '[0]')
-                self.check_forward_hidden(input[0], hx[0][1], '[1]')
-            else:
-                self.check_forward_hidden(input[0], hx[0], '[0]')
-                self.check_forward_hidden(input[0], hx[1], '[1]')
-        else:
-            self.check_forward_input(input)
-            if self.num_layers > 1:
-                self.check_forward_hidden(input, hx[0][0], '[0]')
-                self.check_forward_hidden(input, hx[0][1], '[1]')
-            else:
-                self.check_forward_hidden(input, hx[0], '[0]')
-                self.check_forward_hidden(input, hx[1], '[1]')
+        # if len(input.shape) == 3:
+        #     self.check_forward_input(input[0])
+        #     if self.num_layers > 1:
+        #         self.check_forward_hidden(input[0], hx[0][0], '[0]')
+        #         self.check_forward_hidden(input[0], hx[0][1], '[1]')
+        #     else:
+        #         self.check_forward_hidden(input[0], hx[0], '[0]')
+        #         self.check_forward_hidden(input[0], hx[1], '[1]')
+        # else:
+        #     self.check_forward_input(input)
+        #     if self.num_layers > 1:
+        #         self.check_forward_hidden(input, hx[0][0], '[0]')
+        #         self.check_forward_hidden(input, hx[0][1], '[1]')
+        #     else:
+        #         self.check_forward_hidden(input, hx[0], '[0]')
+        #         self.check_forward_hidden(input, hx[1], '[1]')
 
         # Initialize batchnorm layers
         if self.layer_norm and self.lst_bnorm_rnn is None:
